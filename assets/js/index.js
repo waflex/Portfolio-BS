43510041fb02a524e33fd8046c27f6ea077142b4
Jonathan Rojas <wafle48@gmail.com>
2024-08-26 11:22:16 -0400

    Agregadas imagenes y animacion para titulo

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,16 +4,24 @@ const proyectos = [
         descripcion:
             "Gestiona productos, controla stock en tiempo real, envía alertas de bajo inventario, y genera informes detallados. Integra con otros sistemas para una gestión eficiente.",
         url: "https://enlace-a-sistema-inventario.com", // Cambiar cuando necesario
+        langs: ["Javascript", "HTML", "CSS", "Express", "Handlebars"],
+        imagen: "assets/img/proyectos/inventario.png" // Ruta de la imagen del proyecto
     },
     {
         nombre: "Ficha Veterinaria",
         descripcion:
             "Administra registros de mascotas, historial médico, y citas. Ofrece alertas para vacunas y exámenes, y genera informes detallados sobre la salud y el bienestar de los animales.",
         url: "https://enlace-a-ficha-veterinaria.com", // Cambiar cuando necesario
-    },{
-        nombre:"Bot Discord",
-        descripcion:"Bot designado para la administracion de usuarios y funcionalidades dentro de un servidor de discord, inspirador por completo en la tematica DnD",
-        url:"https://enlace-a-ficha-veterinaria.com",
+        langs: ["Javascript", "HTML", "CSS", "Express", "React"],
+        imagen: "assets/img/proyectos/Ficha-Vet.png" // Ruta de la imagen del proyecto
+    },
+    {
+        nombre: "Bot Discord",
+        descripcion:
+            "Bot designado para la administración de usuarios y funcionalidades dentro de un servidor de Discord, inspirado por completo en la temática DnD.",
+        url: "https://enlace-a-ficha-veterinaria.com",
+        langs: ["Javascript"],
+        imagen: "assets/img/Proyectos/Bot_Discord.png" // Ruta de la imagen del proyecto
     }
 ];
 function cargarProyectos() {
@@ -21,14 +29,22 @@ function cargarProyectos() {
     container.innerHTML = ''; // Limpiar el contenedor antes de agregar nuevos proyectos
 
     proyectos.forEach(proyecto => {
-        console.log(proyecto)
+        // Crear la lista de íconos de lenguajes
+        let langsIcons = proyecto.langs.map(lang => {
+            return `<img src="assets/img/icons/${lang.toLowerCase()}.png" alt="${lang}" class="lang-icon" />`;
+        }).join(' ');
+
         const card = document.createElement('div');
         card.className = 'col-md-4 mb-4';
         card.innerHTML = `
-            <div class="card text-bg-dark">
+            <div class="card text-bg-dark h-75">
+                <img src="${proyecto.imagen}" class="card-img-top" alt="${proyecto.nombre}">
                 <div class="card-body">
                     <h4 class="card-title">${proyecto.nombre}</h4>
                     <p class="card-text">${proyecto.descripcion}</p>
+                    <div class="langs-icons mb-3">
+                        ${langsIcons}
+                    </div>
                     <button
                         class="btn btn-primary"
                         data-bs-toggle="modal"
