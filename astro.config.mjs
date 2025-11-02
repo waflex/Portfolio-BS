d94575a997d7ac3cc97d8bb339e72f3c3d630c7e
Jonathan Rojas <38105133+waflex@users.noreply.github.com>
2025-11-02 17:37:24 -0300

    cambio de formulario

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  vite: {
+    define: {
+      'import.meta.env.PUBLIC_WEB3FORMS_KEY': JSON.stringify(process.env.Web3Forms_KEY)
+    }
+  }
 });
