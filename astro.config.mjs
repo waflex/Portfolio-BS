5f2326d45bf19fee76ee862798b23b61ee387c34
Jonathan Rojas <38105133+waflex@users.noreply.github.com>
2025-11-02 19:46:20 -0300

    actualizado cv

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  outDir: './dist',
+  base: '/',
+  output: 'static',
   vite: {
     define: {
       'import.meta.env.PUBLIC_WEB3FORMS_KEY': JSON.stringify(process.env.Web3Forms_KEY)
-    }
-  }
+    },
+    build: {
+      assetsDir: 'assets', // Asegura que las rutas sean coherentes
+    },
+  },
+  integrations: [tailwind()],  
 });
